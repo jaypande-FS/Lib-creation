8fe34f1f8de534c23a7536962dbeefe101eb93a6
dheeraj2singh <dheeraj.2.singh@coforge.com>
2022-01-26 16:53:40 +0530

    update style of lib-button in src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { TestLibModule } from 'test-lib';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, TestLibModule],
   providers: [],
   bootstrap: [AppComponent],
 })
